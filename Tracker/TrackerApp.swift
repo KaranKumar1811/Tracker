4bfd57b0c3c81fa06d236561c1b4c21f14fd0b52
Sandeep <Sandeepjangra0100@gmail.com>
2021-05-03 04:23:35 -0400

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Sandeep Jangra on 2021-05-03.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
